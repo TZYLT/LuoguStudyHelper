1f7ed61ff686e63ccf2b27e3182389451a2e3997
TZYLT <TZY143@126.com>
2024-12-31 14:44:18 +0800

    feat(DataAcquisition): 更新GetRecordPage.bat脚本以支持目标用户ID参数并优化日志输出

--- DataAcquisition/GetRecordPage.bat
+++ DataAcquisition/GetRecordPage.bat
@@ -1,7 +1,13 @@
-:: Usage: get.bat [uid] [client_id(cookie)] [record_page] [log_file] 
+:: Usage: get.bat [uid] [client_id(cookie)] [target_user_id] [record_page] [log_file] 
+:: Example: get.bat 123456 123456789abcdefg 114514 1 record.log
+:: Example: get.bat 123456 123456789abcdefg 123456 1
+:: Example: get.bat 123456 123456789abcdefg kkksc03 1 
+
 :: Warning: The log file will contain the client_id(cookie) and do not share it with others.
+:: You have to login with your Luogu cookies to get the other user's record page.
+:: You can add -v manually to see more information about the process.
 if exist %%4% (
-    curl -v -o %~dp0RecordFiles/record%3.json "https://www.luogu.com.cn/record/list?user=%1&_contentOnly=1&page=%3" -H "Cookie :_uid=%1; __client_id=%2; " >> %%4% 
+    curl -o %~dp0RecordFiles/record%4.json "https://www.luogu.com.cn/record/list?user=%3&_contentOnly=1&page=%4" -H "Cookie :_uid=%1; __client_id=%2; " >> %%4% 
 ) else (
-    curl -v -o %~dp0RecordFiles/record%3.json "https://www.luogu.com.cn/record/list?user=%1&_contentOnly=1&page=%3" -H "Cookie :_uid=%1; __client_id=%2; " 
+    curl -o %~dp0RecordFiles/record%4.json "https://www.luogu.com.cn/record/list?user=%3&_contentOnly=1&page=%4" -H "Cookie :_uid=%1; __client_id=%2; " 
 )
